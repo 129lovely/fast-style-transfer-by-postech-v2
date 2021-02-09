76f16e6dd12c7c8ee32381c8900e76e255b0899c
129lovely <129lovely@gmail.com>
2021-02-10 03:00:09 +0900

    update test_cam.py (todo: automatic style changing by interval)

--- test_cam.py
+++ test_cam.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 import tensorflow as tf
 import numpy as np
-import cv2
+import cv2, imutils
 import transform, my_utils  # import transform.py, cam_utils.py
 
 
@@ -30,10 +30,9 @@ def get_camera_shape(cam):
 
 
 models = [
-    {"path": "models/model_eunsook_batch_8/final.ckpt", "img": "style/eunsook.jpg", "artist": "Park Eun Sook",},
     {"path": "models/model_gohg/final.ckpt", "img": "style/gohg.jpg", "artist": "gohg",},
-    {"path": "models/model_unknown02/final.ckpt", "img": "style/unknown02.jpg", "artist": "gohg",},
-    {"path": "models/model_wood/final.ckpt", "img": "style/wood.jpg", "artist": "gohg",},
+    {"path": "models/model_unknown02/final.ckpt", "img": "style/unknown02.jpg", "artist": "unknown",},
+    {"path": "models/model_wood/final.ckpt", "img": "style/wood.jpg", "artist": "wood",},
 ]
 
 opts = {
@@ -44,16 +43,12 @@ opts = {
     "horizontal": 1,
     "num_sec": 10,
 }
-width = 700
-device_id = 0
-disp_width = 1250
-disp_source = 1
-horizontal = 1
-num_sec = 1
+
 
 # config font
-fontpath = "/usr/share/fonts/truetype/nanum/NanumBarunGothicBold.ttf"
-font = ImageFont.truetype(font=fontpath, size=150)
+# fontpath = "/usr/share/fonts/truetype/nanum/NanumBrush.ttf"  # brush
+fontpath = "/usr/share/fonts/truetype/nanum/NanumPen.ttf"  # pen
+font = ImageFont.truetype(font=fontpath, size=200)
 ###################################################################
 
 # create and config tkinter instance
@@ -64,16 +59,14 @@ root.title("PONIX")
 root.attributes("-zoomed", True)
 root.resizable(True, True)
 tk_font = tkFont.Font(root, family="MS Serif", size=12)
+# root.configure(bg="black")
 
-
-def get_text_input():
-    # TODO: get text on the rootdow
-    pass
-
-
-def print_with_text():
-    text = text_input.get("1.0", "end")
-    # TODO: imwrite() with text
+width = 700  # content size
+device_id = 0
+disp_width = 1215
+disp_source = 1
+horizontal = 1
+num_sec = 1
 
 
 # open tensorflow session
@@ -100,13 +93,23 @@ IS_VIDEO_STOP = False
 frame = None
 nm = 0
 
-# TODO show preview
+# TODO if stop the video, then init the print directory -> and then activate print, save button!
 def video_stop():
     print("hello")
     global IS_VIDEO_STOP
     IS_VIDEO_STOP = not IS_VIDEO_STOP
+
+    if IS_VIDEO_STOP == False:
+        btn_print["state"] = tk.DISABLED
+        btn_save["state"] = tk.DISABLED
+    else:
+        btn_print["state"] = tk.NORMAL
+        btn_save["state"] = tk.NORMAL
+
+    # remove all print files
+    if os.path.exists("./print/print.png"):
+        os.remove(r"./print/print.png")
     video_play()
-    print(IS_VIDEO_STOP)
 
 
 def capture():
@@ -116,29 +119,77 @@ def capture():
     nm += 1
 
 
-def print_out():
+def save():
     global frame
     output = theme.get_output(frame)
+
+    ch, cw, _ = frame.shape  # cam shape
+    oh, ow, _ = output.shape  # output display shape
+
+    disp_height = int(disp_width * (oh / ow))
+    h = int((ch / cw) * disp_width * 0.5)
+    w = int((cw / ch) * disp_height * 0.5)
+
+    """
+    here to start the merge output & style image
+    """
+    style_downscale = imutils.resize(theme.get_style(), width=100, inter=cv2.INTER_AREA)
+    x_offset = 590
+    y_offset = 10
+    output[
+        y_offset : y_offset + style_downscale.shape[0], x_offset : x_offset + style_downscale.shape[1]
+    ] = style_downscale
+
+    # write text
+    text = text_input.get("1.0", tk.END)
+
     pr_img = np.zeros((3840, 5120, 3), dtype="uint8") + 255  # resizing
     resizeH = int(3840 * 1)
     img = cv2.resize(output, (5120, resizeH))
     pr_img[:resizeH, :, :] = img
     img_pil = Image.fromarray(pr_img)
+
     draw = ImageDraw.Draw(img_pil)
-    draw.text(
-        (640, 3640), "포항공과대학교 인공지능연구원 방문 기념  {}.{}.{}".format(year, month, day), font=font, fill=(255, 255, 255, 0),
-    )
-    pr_img = np.array(img_pil)
-    cv2.imwrite("./print" + "/" "print.png", pr_img)
+    draw.text((100, 3630), text, font=font, fill=(255, 255, 255))
+    output = np.array(img_pil)
+
+    # save to file for printing
+    cv2.imwrite("./print/print.png", output)
+
+    """
+    update the label image
+    """
+    output = Image.fromarray(cv2.cvtColor(cv2.resize(output, (disp_width, disp_height)), cv2.COLOR_BGR2RGB))
+    imgtk_output = ImageTk.PhotoImage(image=output)
+
+    # opencv video
+    label_output.imgtk = imgtk_output
+    label_output.configure(image=imgtk_output)
+
+
+def print_out():
+    # global frame
+    # output = theme.get_output(frame)
+    # pr_img = np.zeros((3840, 5120, 3), dtype="uint8") + 255  # resizing
+    # resizeH = int(3840 * 1)
+    # img = cv2.resize(output, (5120, resizeH))
+    # pr_img[:resizeH, :, :] = img
+    # img_pil = Image.fromarray(pr_img)
+    # draw = ImageDraw.Draw(img_pil)
+    # draw.text(
+    #     (640, 3640), "포항공과대학교 인공지능연구원 방문 기념  {}.{}.{}".format(year, month, day), font=font, fill=(255, 255, 255, 0),
+    # )
+    # pr_img = np.array(img_pil)
+    # cv2.imwrite("./print" + "/" "print.png", pr_img)
     os.system("lpr ./print/print.png")
 
 
 # config frames
 frame_top = tk.Frame(root)
 frame_top.pack(side=tk.TOP, fill=tk.X)
-frame_left = tk.Frame(root, bg="white")
+frame_left = tk.Frame(root)
 frame_left.pack(side=tk.LEFT, fill=tk.Y)
-frame_right = tk.Frame(root, bg="black")
+frame_right = tk.Frame(root)
 frame_right.pack(side=tk.RIGHT)
 
 # config widgets
@@ -150,14 +201,16 @@ btn_next = tk.Button(frame_top, width=10, text="▶", command=lambda: change_sty
 btn_next.pack(side=tk.LEFT)
 btn_capture = tk.Button(frame_top, width=20, text="Capture", command=capture)
 btn_capture.pack(side=tk.LEFT)
-btn_print = tk.Button(frame_top, width=20, text="Print", command=print_out)
+
+# print button
+btn_print = tk.Button(frame_top, width=20, text="Print", command=print_out, state=tk.DISABLED)
 btn_print.pack(side=tk.LEFT)
-btn_save = tk.Button(frame_top, width=20, text="Save")
+btn_save = tk.Button(frame_top, width=20, text="Save", command=save, state=tk.DISABLED)
 btn_save.pack(side=tk.LEFT)
 text_input = tk.Text(frame_top, height=2, font=tk_font)
 text_input.pack(side=tk.LEFT)
 text_artist = tk.Label(frame_top, font=tk_font, text=models[theme.idx]["artist"])
-text_artist.pack(side=tk.RIGHT)
+text_artist.pack(side=tk.LEFT, padx=10)
 
 label_content = tk.Label(frame_left)
 label_content.grid()
@@ -184,12 +237,16 @@ def video_play():
         oh, ow, _ = output.shape  # output display shape
 
         disp_height = int(disp_width * (oh / ow))
-        h = int((ch / cw) * disp_width * 0.5)
-        w = int((cw / ch) * disp_height * 0.5)
+        # h = int((ch / cw) * disp_width * 0.5)
+        # w = int((cw / ch) * disp_height * 0.5)
+        w = int(root.winfo_screenwidth() / 2 - cam_width)
+        h = int(w * (oh / ow))
+        print("w, h", disp_width, disp_height)
 
         content = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         output = Image.fromarray(cv2.cvtColor(cv2.resize(output, (disp_width, disp_height)), cv2.COLOR_BGR2RGB))
-        style = Image.open(models[theme.idx]["img"]).resize((700, 528), Image.ANTIALIAS)
+        # style = Image.open(models[theme.idx]["img"]).resize((700, 528), Image.ANTIALIAS)
+        style = Image.fromarray(cv2.cvtColor(imutils.resize(theme.get_style(), height=500), cv2.COLOR_BGR2RGB))
 
         imgtk_content = ImageTk.PhotoImage(image=content)
         imgtk_output = ImageTk.PhotoImage(image=output)
@@ -216,7 +273,7 @@ def video_play():
 
         content = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         output = Image.fromarray(cv2.cvtColor(cv2.resize(output, (disp_width, disp_height)), cv2.COLOR_BGR2RGB))
-        style = Image.open(models[theme.idx]["img"]).resize((700, 528), Image.ANTIALIAS)
+        style = Image.fromarray(cv2.cvtColor(imutils.resize(theme.get_style(), height=500), cv2.COLOR_BGR2RGB))
 
         imgtk_content = ImageTk.PhotoImage(image=content)
         imgtk_output = ImageTk.PhotoImage(image=output)
@@ -230,10 +287,6 @@ def video_play():
         label_style.imgtk = imgtk_style
         label_style.configure(image=imgtk_style)
         return
-
-
-def save():
-    pass
 
 
 def change_style(is_prev=True):
