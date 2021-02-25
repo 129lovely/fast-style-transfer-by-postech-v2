0764260f7d1c732c39f5cc63d7be91a9382ae0a2
miraekiim <74103229+miraekiim@users.noreply.github.com>
2021-02-25 11:36:02 +0900

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,6 @@ gdown --id 1FcO79mp9iKZlFnMDW8GORgivrOnN19_5 --output style.zip
 gdown --id 11868epUqh3uApWfaTxxqqF3fqFkCHznH --output model.zip
 
 unzip -d style style.zip
-unzip -d model model.zip
+unzip -d models model.zip
 
 exit 0
